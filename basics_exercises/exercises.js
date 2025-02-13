75da662ac1b5ec697b124224e59d7d4df1c94cd5
Amaad <syed.amaad99@gmail.com>
2025-02-13 09:46:14 +0000

    Completed Basics Chapter Exercises.

---
+++ basics_exercises/exercises.js
@@ -0,0 +1,59 @@
+// 1
+console.log('Amaad ' + 'Syed');
+
+//2
+let num = 4936;
+let ones = num % 10;
+num = (num - ones) / 10;
+let tens = num % 10;
+num = (num - tens) / 10;
+let hundreds = num % 10;
+num = (num - hundreds) / 10;
+let thousands = num % 10;
+console.log(ones);
+console.log(tens);
+console.log(hundreds);
+console.log(thousands);
+
+//3
+/*
+string
+boolean
+number
+number
+undefined
+object
+*/
+
+//4
+// The code logs 510 because of implicit coercion, which changes the second number into a string.
+
+//5 
+console.log(Number('5') + 10);
+
+//6 
+console.log(`The value of 5 + 10 is ${Number('5') + 10}.`);
+
+//7
+// No, the code logs undefined.
+
+//8
+let names = ['asta', 'butterscotch', 'pudding', 'neptune', 'darwin'];
+
+//9 
+let pets = {
+  asta: 'dog',
+  butterscotch: 'cat',
+  pudding: 'cat',
+  neptune: 'fish',
+  darwin: 'lizard'
+};
+
+//10
+// Evaluates to false.
+
+//11 
+// Evaluates to 3.
+
+//12
+// Evaluates to true.

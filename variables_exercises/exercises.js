7f432dcd1b0d41e66159bd8f58e80ce275bdffc8
Amaad <syed.amaad99@gmail.com>
2025-02-13 10:27:24 +0000

    Completed Variables Exercises.

---
+++ variables_exercises/exercises.js
@@ -0,0 +1,11 @@
+//3
+// The code outputs an error due to the variable's block scope.
+
+//4
+// The code outputs an error when trying to reassign the constant variable.
+
+//5
+// The code logs 'bar' because the variable within the block scope is inaccessible.
+
+//6 
+// The program doesn't produce an error, because the "new" constant variable is inaccessible from outside the function block.

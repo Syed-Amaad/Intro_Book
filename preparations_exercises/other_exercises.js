56595269fb7111d4c839ee486385dbf5c7fd293b
Amaad <syed.amaad99@gmail.com>
2025-03-15 04:15:07 +0000

    Reviewed Preparartions Chapter

---
+++ preparations_exercises/other_exercises.js
@@ -0,0 +1,16 @@
+/*
+
+(4a) logs 'bar' to the console
+(4b) undefined, undefiend, returns 'bar'
+
+(5) instance, static, static, instance, instance
+
+(6) index, lazyDog, operand2
+
+(7) index, lazyDog, operand2, CatName
+
+(8) BIG_NUMBER
+
+(9) snake_case, quick_Fox, 1stCharacter
+
+*/

fb37bd562e46027468c4f143e19d34e67aa9154f
Amaad <syed.amaad99@gmail.com>
2025-03-15 07:44:41 +0000

    Reviewed Variables Chapter

--- variables_exercises/age.js
+++ variables_exercises/age.js
@@ -1,4 +1,4 @@
-let age = 20
+let age = 25
 console.log(`You are ${age} years old.`);
 console.log(`In 10 years, you will be ${age + 10} years old.`);
 console.log(`In 20 years, you will be ${age + 20} years old.`);
---
+++ variables_exercises/other_exercises.js
@@ -0,0 +1,15 @@
+/*
+
+(3)
+The code outputs an error due to the variable being out of scope.
+
+(4)
+The program runs the first part, then outputs an error when trying to reassign the constant variable.
+
+(5) 
+The code logs 'bar' because the variable that is declared within the block scope is inaccessible.
+
+(6) 
+The program doesn't produce an error, because a new constant variable was declared within the function block.
+
+*/

ec2db898e6b1b7201c6f44e7153c74d5553a4a9c
Amaad <syed.amaad99@gmail.com>
2025-03-23 08:24:42 +0000

    Reviewed Functions Chapter

--- functions_exercises/exercises.js
+++ functions_exercises/exercises.js
@@ -1,14 +1,15 @@
-// 1 
-// logs 1 to the console, because a new variable is scoped within the function body
-
-//4
-// logs nothing
-
-//5
-// logs nothing
-
-//6
 /*
+
+(1) 
+logs 1 to the console, because a new variable bar is scoped within the function body
+
+(4)
+ logs nothing
+
+(5)
+logs nothing
+
+(6)
 arguments are 2,3,4
 body is between the brackets
 the declatation is the code on lines 1 to 4
@@ -17,20 +18,21 @@ the name is multiplyNumbers on line 1
 the parameters are num1, num2, num3
 the return value is 24
 the variable are multiplyNumbers, num1, num2, num3, result, and product
+
+(7)
+outputs 'hello', then outputs undefined
+
+8
+outputs 42 and 3.1415
+
+9
+(line 1: multiply, left, right) (line 2: product, left right) (line 3: product) (line 6: getNumber, prompt) (line 7: parseFloat, question, prompt) (line 10: left, getNumber) (line 11: right, getNumber) (line 12: left, right, multiply, console)
+
+10
+global: left, right, getNumber, multiply, console, parseFloat, question
+local: product, left, right, prompt
+
+11
+these variables are not the same, they are defined with let outside of the function
+
 */
-
-//7
-//outputs 'hello', then outputs undefined
-
-//8
-//outputs 42 and 3.1415
-
-//9
-// (line 1: multiply, left, right) (line 2: product, left right) (line 3: product) (line 6: getNumber, prompt) (line 7: parseFloat, question, prompt) (line 10: left, getNumber) (line 11: right, getNumber) (line 12: left, right, multiply, console)
-
-//10
-//global: left, right, getNumber, multiply, console, parseFloat, question
-//local: product, left, right, prompt
-
-//11
-//these variables are not the same, they are defined with let outside of the function
